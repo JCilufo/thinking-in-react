6aba9fa935e920480ba5951835bd17f5bc7c5ce1
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-11-04 14:36:45 -0600

    docs: add comment to explain the purpose of the constant function

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,7 @@
 import products from "./db.js";
 import { parsePrice } from "./lib.js";
 
+// Finds the highest price from the list of products
 export const HIGHEST_PRICE_OF_PRODUCTS = Math.max(
   ...products.map((product) => parsePrice(product.price))
 );
